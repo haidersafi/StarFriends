8c6827c91a68927a7c16df1333100d36be4c3762
Safi Saqlain <safisaqlain@gmail.com>
2019-10-22 22:58:30 +0500

    Updates

---
+++ precache-manifest.5a777b245bc697ea2a82c78185e31d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922c2e4a11ff71b557dd3165dd2f5873",
+    "url": "/StarFriends/index.html"
+  },
+  {
+    "revision": "21be1eafbeac3c7bb3e3",
+    "url": "/StarFriends/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e01a389b0cf9fe8d57ca",
+    "url": "/StarFriends/static/js/2.01feb8d9.chunk.js"
+  },
+  {
+    "revision": "21be1eafbeac3c7bb3e3",
+    "url": "/StarFriends/static/js/main.2800e76d.chunk.js"
+  },
+  {
+    "revision": "a0913e1283552be9c43d",
+    "url": "/StarFriends/static/js/runtime-main.9001b758.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/StarFriends/static/media/logo.25bf045c.svg"
+  }
+]);
